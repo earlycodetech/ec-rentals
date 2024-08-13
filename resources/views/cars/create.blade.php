a7de7b157c3922ff626dd672a985b1b283a0c19d
emma2625 <emmanuelodobo10@gmail.com>
2024-08-13 16:38:03 +0100

    Updated Form

--- resources/views/cars/create.blade.php
+++ resources/views/cars/create.blade.php
@@ -7,31 +7,61 @@
             </div>
 
             <div class="card-body">
-                <form action="" method="post">
+                <form action="" method="post" enctype="multipart/form-data">
                     <div class="mb-3">
                         <label for="" class="form-label">Name</label>
                         <input type="text" name="name" class="form-control" required>
+                        @error('name')
+                            <p class="text-danger small fw-bold">
+                                <i class="fa-solid fa-exclamation-triangle"></i>
+                                <span>{{ $message }}</span>
+                            </p>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="" class="form-label">Model</label>
                         <input type="text" name="model" class="form-control" required>
+                        @error('model')
+                            <p class="text-danger small fw-bold">
+                                <i class="fa-solid fa-exclamation-triangle"></i>
+                                <span>{{ $message }}</span>
+                            </p>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="" class="form-label">Price</label>
                         <input type="number" name="price" class="form-control" required>
+                        @error('price')
+                            <p class="text-danger small fw-bold">
+                                <i class="fa-solid fa-exclamation-triangle"></i>
+                                <span>{{ $message }}</span>
+                            </p>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="" class="form-label">Quantity</label>
                         <input type="number" name="quantity" class="form-control" required>
+                        @error('quantity')
+                            <p class="text-danger small fw-bold">
+                                <i class="fa-solid fa-exclamation-triangle"></i>
+                                <span>{{ $message }}</span>
+                            </p>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="" class="form-label">Cover</label>
                         <input type="file" name="image" class="form-control" required>
+                        @error('image')
+                            <p class="text-danger small fw-bold">
+                                <i class="fa-solid fa-exclamation-triangle"></i>
+                                <span>{{ $message }}</span>
+                            </p>
+                        @enderror
                     </div>
 
                     <div class="mb-3">
                         <button class="btn btn-primary px-5">
-                            Save 
+                            Save
                         </button>
                     </div>
                 </form>
