2a9ff18416ec6e877fa60509d0cda494dda33835
emma2625 <emmanuelodobo10@gmail.com>
2024-08-20 16:32:25 +0100

    Class Update

--- app/Models/Rental.php
+++ app/Models/Rental.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Rental extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'car_id',
+        'name',
+        'phone',
+        'quantity',
+        'return_date',
+        'is_returned'
+    ];
+
+    protected $casts = [
+        'price' => "decimal:2",
+        'return_date' => 'date',
+        'is_returned' => "boolean"
+    ]
 }
